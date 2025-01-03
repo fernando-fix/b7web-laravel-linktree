15ed3f6fd41daa28bbbbcdacb7bc9036b751f064
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2025-01-02 23:19:59 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Página principal
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Autnticação e registro
+Route::get('register', [UserController::class, 'create'])->name('register');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('login', [AuthController::class, 'store'])->name('logout');
+Route::post('logout/{id}', [AuthController::class, 'destroy'])->name('logout');
+Route::resource('users', UserController::class);
+
+Route::group(
+    ['middleware' => 'auth'],
+    function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('links', LinkController::class);
+    }
+);
